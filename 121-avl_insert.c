2af973b124f3f584e86da411389bb96f253c54c3
codeabuu <abdulngui718@gmail.com>
2023-08-28 16:09:10 -0700

    function that inserts a value in an AVL Tree

---
+++ 121-avl_insert.c
@@ -0,0 +1,74 @@
+#include "binary_trees.h"
+
+/**
+ * r_insert_node - node value instertion in a AVL.
+ * @tree: type **pointer of root node of the AVL tree struct.
+ * @parent: parent node of struct AVL.
+ * @new: type**pointer left or right insertion.
+ * @nval: insertion value of the AVL.
+ * Return: pointer to the new root after insertion otherwise NULL
+ */
+avl_t *r_insert_node(avl_t **tree, avl_t *parent, avl_t **new, int nval)
+{
+	int bval = binary_tree_balance(*tree);
+
+	if (*tree == NULL)
+	{
+		*new = binary_tree_node(parent, nval);
+		return (*new);
+	}
+	if ((*tree)->n > nval)
+	{
+		(*tree)->left = r_insert_node(&(*tree)->left, *tree, new, nval);
+		if ((*tree)->left == NULL)
+			return (NULL);
+	}
+	else if ((*tree)->n < nval)
+	{
+		(*tree)->right = r_insert_node(&(*tree)->right, *tree, new, nval);
+		if ((*tree)->right == NULL)
+			return (NULL);
+	}
+	else
+	{
+		return (*tree);
+	}
+
+	if (bval > 1 && (*tree)->left->n > nval)
+	{
+		*tree = binary_tree_rotate_right(*tree);
+	}
+	else if (bval > 1 && (*tree)->left->n < nval)
+	{
+		(*tree)->left = binary_tree_rotate_left((*tree)->left);
+		*tree = binary_tree_rotate_right(*tree);
+	}
+	else if (bval < -1 && (*tree)->right->n < nval)
+	{
+		*tree = binary_tree_rotate_left(*tree);
+	}
+	else if (bval < -1 && (*tree)->right->n > nval)
+	{
+		(*tree)->right = binary_tree_rotate_right((*tree)->right);
+		*tree = binary_tree_rotate_left(*tree);
+	}
+	return (*tree);
+}
+/**
+ * avl_insert - inserts a value into an AVL tree.
+ * @tree: type **pointer to the root node of the AVL tree to insert into.
+ * @value: value to store in the node to be inserted
+ * Return: inserted node, or NULL if fails.
+ */
+avl_t *avl_insert(avl_t **tree, int value)
+{
+	avl_t *new;
+	new = NULL;
+
+	if (*tree == NULL)
+	{
+		return (*tree = binary_tree_node(NULL, value));
+	}
+	r_insert_node(tree, *tree, &new, value);
+	return (new);
+}
