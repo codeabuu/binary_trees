c9709480421d86b5f9ec9d8180b6d8125919649e
codeabuu <abdulngui718@gmail.com>
2023-08-28 03:45:22 -0700

    create nnimary tree code

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a new binary tree node
+ *
+ * @parent: - is a pointer to the parent node of the node to create
+ * @value: - is the value to put in the new node
+ * Return: a pointer to the new node, or NULL on failure
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+
+	newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+
+	newNode->parent = parent;
+	newNode->n = value;
+	newNode->left = newNode->right = NULL;
+
+	return (newNode);
+}
