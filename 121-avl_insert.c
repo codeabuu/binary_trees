877163be478461a54ca2ceaecfac2cfbae89219a
codeabuu <abdulngui718@gmail.com>
2023-08-29 10:35:06 -0700

    updates

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,21 +1,21 @@
 #include "binary_trees.h"
 
 /**
-* r_insert_node - node value instertion in a AVL.
-* @tree: type **pointer of root node of the AVL tree struct.
-* @parent: parent node of struct AVL.
-* @new: type**pointer left or right insertion.
-* @nval: insertion value of the AVL.
-* Return: pointer to the new root after insertion otherwise NULL
-*/
+ * r_insert_node - node value instertion in a AVL.
+ * @tree: type **pointer of root node of the AVL tree struct.
+ * @parent: parent node of struct AVL.
+ * @new: type**pointer left or right insertion.
+ * @nval: insertion value of the AVL.
+ * Return: pointer to the new root after insertion otherwise NULL
+ */
 avl_t *r_insert_node(avl_t **tree, avl_t *parent, avl_t **new, int nval)
 {
-	int bval = binary_tree_balance(*tree);
+	int bval;
 
-	if (*tree == NULL)
+	if (!*tree)
 	{
 		*new = binary_tree_node(parent, nval);
-		return (*new);
+		return (new)
 	}
 	if ((*tree)->n > nval)
 	{
@@ -33,7 +33,7 @@ avl_t *r_insert_node(avl_t **tree, avl_t *parent, avl_t **new, int nval)
 	{
 		return (*tree);
 	}
-
+	bval = binary_tree_balance(*tree);
 	if (bval > 1 && (*tree)->left->n > nval)
 	{
 		*tree = binary_tree_rotate_right(*tree);
@@ -55,19 +55,19 @@ avl_t *r_insert_node(avl_t **tree, avl_t *parent, avl_t **new, int nval)
 	return (*tree);
 }
 /**
-* avl_insert - inserts a value into an AVL tree.
-* @tree: type **pointer to the root node of the AVL tree to insert into.
-* @value: value to store in the node to be inserted
-* Return: inserted node, or NULL if fails.
-*/
+ * avl_insert - inserts a value into an AVL tree.
+ * @tree: type **pointer to the root node of the AVL tree to insert into.
+ * @value: value to store in the node to be inserted
+ * Return: inserted node, or NULL if fails.
+ */
 avl_t *avl_insert(avl_t **tree, int value)
 {
-	avl_t *new;
-	new = NULL;
+	avl_t *new = NULL;
 
 	if (*tree == NULL)
 	{
-		return (*tree = binary_tree_node(NULL, value));
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
 	}
 	r_insert_node(tree, *tree, &new, value);
 	return (new);
