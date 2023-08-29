11ab7ba42a4634a421199c43bf5c6a32ff505534
codeabuu <abdulngui718@gmail.com>
2023-08-29 10:24:01 -0700

    updates

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
-	if (newNode == NULL)
+	if (!newNode)
 	{
 		return (NULL);
 	}
